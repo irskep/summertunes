bc9f70c5793bfb279bbd94c83da05d70538ec502
Steve Johnson <steve@graffitilabs.com>
2016-12-20 13:24:06 -0800

    More CSS

--- client/src/Table.js
+++ client/src/Table.js
@@ -5,35 +5,33 @@ class Table extends Component {
   render() {
     const className = `${this.props.className} noselect st-table`;
     return (
-      <div className={className} style={{overflow: 'auto', position: 'relative'}}>
-        <div className="st-table-abs-container">
-          <table>
-            <thead>
-              <tr>
-                {this.props.columns.map(({name, itemKey}) => (
-                  <th key={`${itemKey}-${name}`}>{name}</th>
-                ))}
-              </tr>
-            </thead>
-            <tbody>
-              {this.props.items.map((item, i) => {
-                return (
-                  <tr key={i}
-                      className={this.props.selectedItem === item ? "st-table-item-selected" : ""}
-                      onClick={() => this.props.onClick(item, i)}>
-                    {this.props.columns.map((column) => (
-                      <td key={`${column.itemKey}-${column.name}`}>
-                        {column.itemKey === 'func'
-                          ? column.func(item)
-                          : item[column.itemKey]}
-                      </td>
-                    ))}
-                  </tr>
-                );
-              })}
-            </tbody>
-          </table>
-        </div>
+      <div className={className}>
+        <table>
+          <thead>
+            <tr>
+              {this.props.columns.map(({name, itemKey}) => (
+                <th key={`${itemKey}-${name}`}>{name}</th>
+              ))}
+            </tr>
+          </thead>
+          <tbody>
+            {this.props.items.map((item, i) => {
+              return (
+                <tr key={i}
+                    className={this.props.selectedItem === item ? "st-table-item-selected" : ""}
+                    onClick={() => this.props.onClick(item, i)}>
+                  {this.props.columns.map((column) => (
+                    <td key={`${column.itemKey}-${column.name}`}>
+                      {column.itemKey === 'func'
+                        ? column.func(item)
+                        : item[column.itemKey]}
+                    </td>
+                  ))}
+                </tr>
+              );
+            })}
+          </tbody>
+        </table>
       </div>
     );
   }
