b4d45f09cfb8192a70ac6532cbffae4723ef27cf
Steve Johnson <steve@graffitilabs.com>
2017-01-29 17:17:34 -0800

    Successfully serve summertunes from beet web

--- beetsplug/summertunes.py
+++ beetsplug/summertunes.py
@@ -6,4 +6,11 @@ from summertunes.routes import summertunes_routes
 class SummertunesPlugin(BeetsPlugin):
     def __init__(self):
         super(SummertunesPlugin, self).__init__()
-beetsweb_app.register_blueprint(summertunes_routes)
+        beetsweb_app.config['SERVER_CONFIG'] = {
+            'MPV_PORT': 3001,
+            'BEETSWEB_PORT': 8337,
+            'SUMMERTUNES_PORT': 8337,
+            'player_services': ['web', 'mpv'],
+            'LAST_FM_API_KEY': "",
+        }
+beetsweb_app.register_blueprint(summertunes_routes, url_prefix="/summertunes")
